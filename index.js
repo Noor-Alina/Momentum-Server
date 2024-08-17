dc5b4b64c6c9948986ff17aeeccb7b29da7d52c3
Alina Noor <AN10_23@hotmail.com>
2024-08-16 23:25:19 -0400

    create base routing for auth and profiles

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+import "dotenv/config";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import profileRoutes from "./routes/profile.js";
+
+
+const PORT = process.env.PORT || 8081;
+
+app.use(cors());
+app.use(express.json());
+
+//base for auth routes
+app.use('/', authRoutes);
+
+//base for profile route
+app.use('/user', profileRoutes);
+
+app.listen(PORT, () => {
+console.log(`Listening on PORT ${PORT}.`);
+});
