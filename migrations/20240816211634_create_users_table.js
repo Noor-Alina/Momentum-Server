8d9614850f1ab9a824d19d7ff0ce404637f86a2a
Alina Noor <AN10_23@hotmail.com>
2024-08-16 23:22:51 -0400

    setup knexfile configuration and users migration

---
+++ migrations/20240816211634_create_users_table.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    console.log("Running migration: create_users_table");
+    return knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('user_name').notNullable();
+      table.string('user_email').notNullable().unique();
+      table.string('password').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    });
+  };
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
