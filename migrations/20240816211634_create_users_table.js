09219ff1a851c059697be1fd123ca92b31653762
Alina Noor <AN10_23@hotmail.com>
2024-08-18 11:48:37 -0400

    remove console log fron users migration file

--- migrations/20240816211634_create_users_table.js
+++ migrations/20240816211634_create_users_table.js
@@ -4,7 +4,6 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-    console.log("Running migration: create_users_table");
     return knex.schema.createTable('users', (table) => {
       table.increments('id').primary();
       table.string('user_name').notNullable();
