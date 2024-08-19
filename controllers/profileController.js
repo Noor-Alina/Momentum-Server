8b78d824f98a2e7d9b804dc56ba2417be488f696
Alina Noor <AN10_23@hotmail.com>
2024-08-18 23:14:58 -0400

    create an update function for profile to update user info and create the route

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -23,4 +23,35 @@ const getProfile = async (req, res) => {
     }
 };
 
-export {getProfile};
+//update
+const updateProfile = async (req, res) => {
+    const userId = req.userId; 
+    const { user_name, user_email, password } = req.body;
+
+    try {
+        // Validate input data
+        if (!user_name || !user_email) {
+            return res.status(400).json({ error: 'Name and email are required' });
+        }
+
+        const updateData = { user_name, user_email };
+        if (password) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            updateData.password = hashedPassword;
+        }
+
+        const updatedUser = await knex('users').where({ id: userId }).update(updateData);
+        if (updatedUser) {
+           return  res.status(200).json({ message: 'Profile updated successfully' });
+        } else {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        
+    } catch (error) {
+            console.error('Error updating profile:', error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+
+};
+
+export {getProfile, updateProfile};
--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getProfile } from '../controllers/profileController.js';
+import { getProfile, updateProfile } from '../controllers/profileController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Protected route to get user profile
 router.get('/profile', authMiddleware, getProfile);
+
+router.put('/profile', authMiddleware, updateProfile);
 
 export default router;
