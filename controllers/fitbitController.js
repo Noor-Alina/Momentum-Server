60e6940f0bbb8cc53554e33bb38797c2011e5e7a
Alina Noor <AN10_23@hotmail.com>
2024-09-17 13:52:18 -0400

    remove comments and console logs

--- controllers/fitbitController.js
+++ controllers/fitbitController.js
@@ -8,18 +8,12 @@ const authorize = (req, res) => {
 
     const fitbitAuthUrl = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${FITBIT_CLIENT_ID}&redirect_uri=${FITBIT_REDIRECT_URI}&scope=profile%20activity%20nutrition`;
     
-    console.log("Authorization URL:", fitbitAuthUrl); // Log the URL for debugging
     res.redirect(fitbitAuthUrl);
 };
 
 const callback = async (req, res) => {
-    console.log("Callback query parameters:", req.query);
 
     const {code} = req.query; 
-    console.log("code:", code);
-
-    console.log("Redirect URI:", FITBIT_REDIRECT_URI);
-
     try {
         const tokenResponse = await axios.post('https://api.fitbit.com/oauth2/token', null, {
             params: {
@@ -38,8 +32,6 @@ const callback = async (req, res) => {
         });
 
         const {access_token} = tokenResponse.data;
-        console.log("Token Response:", tokenResponse.data);
-        console.log("Access Token:", access_token); 
         res.redirect(`http://localhost:5173/Tracking?access_token=${access_token}`);
     } catch (error) {
         console.error('Error getting access token:', error.response ? error.response.data : error.message);
@@ -57,7 +49,7 @@ const getData = async (req, res) => {
     } else{
 
         try {
-            const [profileResponse, activityResponse, nutritionResponse, sleepResponse] 
+            const [profileResponse, activityResponse, nutritionResponse] 
             = await Promise.all([
 
                 axios.get('https://api.fitbit.com/1/user/-/profile.json', {
@@ -72,19 +64,12 @@ const getData = async (req, res) => {
                     headers: { Authorization: `Bearer ${accessToken}` },
                 }),
 
-                // axios.get('https://api.fitbit.com/1.2/user/-/sleep/date/2024-04-17.json', {
-                //     headers: { Authorization: `Bearer ${accessToken}` },
-                // }),
             ]);
 
-            //for perticular day - https://api.fitbit.com/1/user/-/foods/log/date/2024-01-01.json
-
-            // console.log('Sleep:', sleepResponse.data);
             res.json({
                 profile: profileResponse.data,
                 activity: activityResponse.data,
                 nutrition: nutritionResponse.data,
-                // sleep: sleepResponse.data,
             });
         } catch (error) {
             console.error('Error fetching data:', error);
