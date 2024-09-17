ca874767772849ba06408690239898018a1436a5
Alina Noor <AN10_23@hotmail.com>
2024-09-17 13:25:37 -0400

    export fitbit env variables

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,6 @@
 export const SECRET_KEY = process.env.JWT_KEY ?? "secret_key";
 export const GEMINI_API_KEY = process.env.API_KEY ?? "gemini_key";
+
+export const FITBIT_CLIENT_ID = process.env.FITBIT_CLIENT_ID ?? "client_id";
+export const FITBIT_CLIENT_SECRET = process.env.FITBIT_CLIENT_SECRET ?? "fitbit_key";
+export const FITBIT_REDIRECT_URI = process.env.FITBIT_REDIRECT_URI ?? "redirect_uri";
