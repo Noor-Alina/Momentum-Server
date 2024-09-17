382537873dc105259ceee713f266c31e0fa24314
Alina Noor <AN10_23@hotmail.com>
2024-09-17 13:46:09 -0400

    implement call back function which on token retrieval redirects back to the calling url with the access token

--- controllers/fitbitController.js
+++ controllers/fitbitController.js
@@ -12,3 +12,39 @@ const authorize = (req, res) => {
     res.redirect(fitbitAuthUrl);
 };
 
+const callback = async (req, res) => {
+    console.log("Callback query parameters:", req.query);
+
+    const {code} = req.query; 
+    console.log("code:", code);
+
+    console.log("Redirect URI:", FITBIT_REDIRECT_URI);
+
+    try {
+        const tokenResponse = await axios.post('https://api.fitbit.com/oauth2/token', null, {
+            params: {
+                client_id: FITBIT_CLIENT_ID,
+                grant_type: 'authorization_code',
+                redirect_uri: FITBIT_REDIRECT_URI,
+                code: code,
+
+            },
+
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${FITBIT_CLIENT_ID}:${FITBIT_CLIENT_SECRET}`).toString('base64')}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            
+        });
+
+        const {access_token} = tokenResponse.data;
+        console.log("Token Response:", tokenResponse.data);
+        console.log("Access Token:", access_token); 
+        res.redirect(`http://localhost:5173/Tracking?access_token=${access_token}`);
+    } catch (error) {
+        console.error('Error getting access token:', error.response ? error.response.data : error.message);
+        res.status(500).send('Authentication failed');
+    }
+};
+
+export {authorize, callback};
