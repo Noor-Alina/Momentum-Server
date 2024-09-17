c54ebb8469167908fa48af9559750a08daf6f2e6
Alina Noor <AN10_23@hotmail.com>
2024-09-17 13:58:58 -0400

    import fitbit routes and add a base endpoint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 const app = express();
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import path from "path";
+
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profile.js";
 import workoutRoutes from "./routes/workouts.js";
 import geminiRoutes from "./routes/gemini.js";
+import fitbitRoutes from "./routes/fitbit.js";
 
 
 const PORT = process.env.PORT || 8081;
@@ -18,14 +21,14 @@ app.use('/images', express.static(path.resolve('public/images')));
 
 app.use('/auth', authRoutes);
 
-
 app.use('/user', profileRoutes);
-
 
 app.use('/workouts', workoutRoutes);
 
-
 app.use('/gemini', geminiRoutes);
+
+// Use the Fitbit routes
+app.use('/fitbit', fitbitRoutes);
 
 app.listen(PORT, () => {
 console.log(`Listening on PORT ${PORT}.`);
