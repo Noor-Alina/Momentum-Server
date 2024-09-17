15ddf19948e283ec117e7119557c259214b70635
Alina Noor <AN10_23@hotmail.com>
2024-09-17 13:54:28 -0400

    remove comments

--- routes/fitbit.js
+++ routes/fitbit.js
@@ -3,13 +3,10 @@ const router = express.Router();
 import { authorize, callback, getData } from '../controllers/fitbitController.js'; 
 
 
-// Route to initiate OAuth flow
 router.get('/auth', authorize);
 
-// Route to handle OAuth callback
 router.get('/callback', callback);
 
-// Route to fetch Fitbit data
 router.get('/data', getData);
 
 
